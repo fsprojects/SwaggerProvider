f486ac2ffd9b9bd214ac1a245e93f1d82ddad79b
Sergey Tihon <sergey.tihon@gmail.com>
2015-12-06 21:51:28 +0300

    Added release notes

--- tests/SwaggerProvider.ProviderTests/Swagger.GitHub.Tests.fs
+++ tests/SwaggerProvider.ProviderTests/Swagger.GitHub.Tests.fs
@@ -9,7 +9,7 @@ let [<Literal>] schema = __SOURCE_DIRECTORY__ + "/Schemes/GitHub.json"
 let [<Literal>] headers = "User-Agent=Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405"
 type GitHub = SwaggerProvider<schema, headers>
 
-[<Test>]
+[<Test; Explicit>]
 let ``Get fsprojects from GitHub`` () =
     GitHub.Repos.OrgRepos("fsprojects").Length
     |> should be (greaterThan 0)
