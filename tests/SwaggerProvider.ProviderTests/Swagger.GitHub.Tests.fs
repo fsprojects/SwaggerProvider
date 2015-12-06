1def2c5291c77d70f3d0c761282a285564ee88da
Sergey Tihon <sergey.tihon@gmail.com>
2015-12-06 20:59:04 +0300

    Fix for Mono

--- tests/SwaggerProvider.ProviderTests/Swagger.GitHub.Tests.fs
+++ tests/SwaggerProvider.ProviderTests/Swagger.GitHub.Tests.fs
@@ -5,7 +5,7 @@ open FSharp.Data
 open NUnit.Framework
 open FsUnit
 
-let [<Literal>] schema = __SOURCE_DIRECTORY__ + "\Schemes\GitHub.json"
+let [<Literal>] schema = __SOURCE_DIRECTORY__ + "/Schemes/GitHub.json"
 let [<Literal>] headers = "User-Agent=Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405"
 type GitHub = SwaggerProvider<schema, headers>
 
