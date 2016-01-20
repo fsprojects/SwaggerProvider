d338b68b604bf7a194c8c0087cd570f807cd12a7
Sergey Tihon <sergey.tihon@gmail.com>
2016-01-20 17:12:41 +0300

    Bump version to 0.3.4

--- src/Common/AssemblyInfo.fs
+++ src/Common/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("SwaggerProvider")>]
 [<assembly: AssemblyProductAttribute("SwaggerProvider")>]
 [<assembly: AssemblyDescriptionAttribute("F# Type Provider for Swagger")>]
-[<assembly: AssemblyVersionAttribute("0.3.3")>]
-[<assembly: AssemblyFileVersionAttribute("0.3.3")>]
+[<assembly: AssemblyVersionAttribute("0.3.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.3.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.3.3"
+    let [<Literal>] Version = "0.3.4"
