652b6387ee334d19d7f18f4785294ccb169f2841
Sergey Tihon <sergey.tihon@gmail.com>
2016-04-09 01:59:37 +0300

    Added Instagram schema that could not be compiled yet

---
+++ tests/SwaggerProvider.ProviderTests/Swagger.Instagram.Tests.fs
@@ -0,0 +1,7 @@
+﻿module Swagger.Instagram.Tests
+
+open SwaggerProvider
+
+let [<Literal>] schema = __SOURCE_DIRECTORY__ + "/Schemes/Instagram.json"
+//type Instagram = SwaggerProvider<schema, "Content-Type=application/json">
+
