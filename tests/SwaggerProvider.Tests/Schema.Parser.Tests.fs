5eb98bf27d830d46aed87a5b41f61a3a4b782bff
Sergey Tihon <sergey.tihon@gmail.com>
2016-06-11 12:26:32 +0300

    bypass ssl checks =(

--- tests/SwaggerProvider.Tests/Schema.Parser.Tests.fs
+++ tests/SwaggerProvider.Tests/Schema.Parser.Tests.fs
@@ -62,13 +62,19 @@ let ``Schema parse of PetStore.Swagger.json sample (online)`` () =
 
 // Test that provider can parse real-word Swagger 2.0 schemas
 // https://github.com/APIs-guru/api-models/blob/master/API.md
-type ApisGuru = FSharp.Data.JsonProvider<"https://api.apis.guru/v2/list.json">
+type ApisGuru = FSharp.Data.JsonProvider<"APIs.guru.list.sample.json">
+
+//Accept all ssl certificates regardless of validation
+System.Net.ServicePointManager.ServerCertificateValidationCallback
+    <- fun _ _ _ _ -> true
 
 let toTestCase (url:string) =
     TestCaseData(url).SetName(sprintf "Parse schema %s" url)
 
 let getApisGuruSchemas propertyName =
-    ApisGuru.GetSample().JsonValue.Properties()
+    ApisGuru
+        .Load("https://api.apis.guru/v2/list.json")
+        .JsonValue.Properties()
     |> Array.choose (fun (name, obj)->
         obj.TryGetProperty("versions")
         |> Option.bind (fun v->
