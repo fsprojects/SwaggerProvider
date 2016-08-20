dbec85b2027b94127852d96e0d36d590752f3ef9
Sergey Tihon <sergey.tihon@gmail.com>
2016-08-20 08:30:15 +0300

    F# 3.0 support

--- src/SwaggerProvider.Runtime/RuntimeHelpers.fs
+++ src/SwaggerProvider.Runtime/RuntimeHelpers.fs
@@ -19,7 +19,7 @@ type private OptionConverter() =
 
     override x.WriteJson(writer, value, serializer) =
         let value =
-            if isNull value then null
+            if value = null then null
             else
                 let _,fields = FSharpValue.GetUnionFields(value, value.GetType())
                 fields.[0]
@@ -32,7 +32,7 @@ type private OptionConverter() =
             else innerType
         let value = serializer.Deserialize(reader, innerType)
         let cases = FSharpType.GetUnionCases(t)
-        if isNull value then FSharpValue.MakeUnion(cases.[0], [||])
+        if value = null then FSharpValue.MakeUnion(cases.[0], [||])
         else FSharpValue.MakeUnion(cases.[1], [|value|])
 
 type private ByteArrayConverter() =
