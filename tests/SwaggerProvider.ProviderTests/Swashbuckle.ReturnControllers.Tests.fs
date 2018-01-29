07d100d185e026a5d4adbbd1c341ef0e7f1d4357
Chet Husk <chusk3@gmail.com>
2018-01-29 15:14:15 -0600

    make local dev server tests async for THE SPEED

--- tests/SwaggerProvider.ProviderTests/Swashbuckle.ReturnControllers.Tests.fs
+++ tests/SwaggerProvider.ProviderTests/Swashbuckle.ReturnControllers.Tests.fs
@@ -4,137 +4,160 @@ open Expecto
 open SwaggerProvider
 open System
 
-type WebAPI = SwaggerProvider<"http://localhost:8735/swagger/docs/v1", IgnoreOperationId=true, Synchronous = true>
+type WebAPI = SwaggerProvider<"http://localhost:8735/swagger/docs/v1", IgnoreOperationId=true>
 let api = WebAPI()
 
 let shouldEqual expected actual =
     Expect.equal actual expected "return value"
 
+let asyncEqual expected actual = 
+    async {
+        let! actual' = actual
+        shouldEqual expected actual'
+    }
+
 [<Tests>]
 let returnControllersTests =
   testList "All/Swashbuckle.ReturnControllers.Tests" [
 
-    testCase "Return Bool GET Test" <| fun _ ->
-        api.GetApiReturnBoolean()
-        |> shouldEqual true
+    testCaseAsync "Return Bool GET Test" <| 
+        (api.GetApiReturnBoolean()
+         |> asyncEqual true)
 
-    testCase "Return Bool POST Test" <| fun _ ->
-        api.PostApiReturnBoolean()
-        |> shouldEqual true
-
-    testCase "Return Int32 GET Test" <| fun _ ->
-        api.GetApiReturnInt32()
-        |> shouldEqual 42
-
-    testCase "Return Int32 POST Test" <| fun _ ->
-        api.PostApiReturnInt32()
-        |> shouldEqual 42
+    testCaseAsync "Return Bool POST Test" <| 
+        (api.PostApiReturnBoolean()
+         |> asyncEqual true)
 
 
-    testCase "Return Int64 GET Test" <| fun _ ->
-        api.GetApiReturnInt64()
-        |> shouldEqual 42L
+    testCaseAsync "Return Int32 GET Test" <| 
+        (api.GetApiReturnInt32()
+         |> asyncEqual 42)
 
-    testCase "Return Int64 POST Test" <| fun _ ->
-        api.PostApiReturnInt64()
-        |> shouldEqual 42L
-
-
-    testCase "Return Float GET Test" <| fun _ ->
-        api.GetApiReturnFloat()
-        |> shouldEqual 42.0f
-
-    testCase "Return Float POST Test" <| fun _ ->
-        api.PostApiReturnFloat()
-        |> shouldEqual 42.0f
+    testCaseAsync "Return Int32 POST Test" <| 
+        (api.PostApiReturnInt32()
+         |> asyncEqual 42)
 
 
-    testCase "Return Double GET Test" <| fun _ ->
-        api.GetApiReturnDouble()
-        |> shouldEqual 42.0
+    testCaseAsync "Return Int64 GET Test" <| 
+        (api.GetApiReturnInt64()
+         |> asyncEqual 42L)
 
-    testCase "Return Double POST Test" <| fun _ ->
-        api.PostApiReturnDouble()
-        |> shouldEqual 42.0
-
-
-    testCase "Return String GET Test" <| fun _ ->
-        api.GetApiReturnString()
-        |> shouldEqual "Hello world"
-
-    testCase "Return String POST Test" <| fun _ ->
-        api.PostApiReturnString()
-        |> shouldEqual "Hello world"
+    testCaseAsync "Return Int64 POST Test" <| 
+        (api.PostApiReturnInt64()
+         |> asyncEqual 42L)
 
 
-    testCase "Return DateTime GET Test" <| fun _ ->
-        api.GetApiReturnDateTime()
-        |> shouldEqual (DateTime(2015,1,1))
+    testCaseAsync "Return Float GET Test" <| 
+        (api.GetApiReturnFloat()
+         |> asyncEqual 42.0f)
 
-    testCase "Return DateTime POST Test" <| fun _ ->
-        api.PostApiReturnDateTime()
-        |> shouldEqual (DateTime(2015,1,1))
+    testCaseAsync "Return Float POST Test" <| 
+        (api.PostApiReturnFloat()
+         |> asyncEqual 42.0f)
 
 
+    testCaseAsync "Return Double GET Test" <| 
+        (api.GetApiReturnDouble()
+         |> asyncEqual 42.0)
+
+    testCaseAsync "Return Double POST Test" <| 
+        (api.PostApiReturnDouble()
+         |> asyncEqual 42.0)
+
+
+    testCaseAsync "Return String GET Test" <| 
+        (api.GetApiReturnString()
+         |> asyncEqual "Hello world")
+
+    testCaseAsync "Return String POST Test" <| 
+        (api.PostApiReturnString()
+         |> asyncEqual "Hello world")
+
+
+    testCaseAsync "Return DateTime GET Test" <| 
+        (api.GetApiReturnDateTime()
+         |> asyncEqual (DateTime(2015,1,1)))
+
+    testCaseAsync "Return DateTime POST Test" <| 
+        (api.PostApiReturnDateTime()
+         |> asyncEqual (DateTime(2015,1,1)))
+
+
+    testCaseAsync "Return Enum GET Test" <| 
+        (api.GetApiReturnEnum()
+         |> asyncEqual "1")
+
+<<<<<<< HEAD
+||||||| merged common ancestors
     testCase "Return Enum GET Test" <| fun _ ->
-        api.GetApiReturnEnum()
+        api.PostApiReturnEnum()
         |> shouldEqual "1"
 
+=======
+    testCaseAsync "Return Enum GET Test" <| 
+        (api.PostApiReturnEnum()
+         |> asyncEqual "1")
 
-    testCase "Return Array Int GET Test" <| fun _ ->
-        api.GetApiReturnArrayInt()
-        |> shouldEqual [|1;2;3|]
+>>>>>>> make local dev server tests async for THE SPEED
 
-    testCase "Return Array Int POST Test" <| fun _ ->
-        api.PostApiReturnArrayInt()
-        |> shouldEqual [|1;2;3|]
+    testCaseAsync "Return Array Int GET Test" <| 
+        (api.GetApiReturnArrayInt()
+         |> asyncEqual [|1;2;3|])
 
-
-    testCase "Return Array Enum GET Test" <| fun _ ->
-        api.GetApiReturnArrayEnum()
-        |> shouldEqual [|"1";"2"|]
-
-    testCase "Return Array Enum POST Test" <| fun _ ->
-        api.PostApiReturnArrayEnum()
-        |> shouldEqual [|"1";"2"|]
+    testCaseAsync "Return Array Int POST Test" <| 
+        (api.PostApiReturnArrayInt()
+         |> asyncEqual [|1;2;3|])
 
 
-    testCase "Return List Int GET Test" <| fun _ ->
-        api.GetApiReturnListInt()
-        |> shouldEqual [|1;2;3|]
+    testCaseAsync "Return Array Enum GET Test" <| 
+        (api.GetApiReturnArrayEnum()
+         |> asyncEqual [|"1";"2"|])
 
-    testCase "Return List Int POST Test" <| fun _ ->
-        api.PostApiReturnListInt()
-        |> shouldEqual [|1;2;3|]
-
-
-    testCase "Return Seq Int GET Test" <| fun _ ->
-        api.GetApiReturnSeqInt()
-        |> shouldEqual [|1;2;3|]
-
-    testCase "Return Seq Int POST Test" <| fun _ ->
-        api.PostApiReturnSeqInt()
-        |> shouldEqual [|1;2;3|]
+    testCaseAsync "Return Array Enum POST Test" <| 
+        (api.PostApiReturnArrayEnum()
+         |> asyncEqual [|"1";"2"|])
 
 
-    testCase "Return Object Point GET Test" <| fun _ ->
-        let point = api.GetApiReturnObjectPointClass()
-        point.X |> shouldEqual (Some(0))
-        point.Y |> shouldEqual (Some(0))
+    testCaseAsync "Return List Int GET Test" <| 
+        (api.GetApiReturnListInt()
+         |> asyncEqual [|1;2;3|])
 
-    testCase "Return Object Point POST Test" <| fun _ ->
-        let point = api.PostApiReturnObjectPointClass()
-        point.X |> shouldEqual (Some(0))
-        point.Y |> shouldEqual (Some(0))
+    testCaseAsync "Return List Int POST Test" <| 
+        (api.PostApiReturnListInt()
+         |> asyncEqual [|1;2;3|])
 
 
-    testCase "Return FileDescription GET Test" <| fun _ ->
-        let file = api.GetApiReturnFileDescription()
-        file.Name.Value |> shouldEqual "1.txt"
-        file.Bytes.Value |> shouldEqual [|1uy;2uy;3uy|]
+    testCaseAsync "Return Seq Int GET Test" <| 
+        (api.GetApiReturnSeqInt()
+         |> asyncEqual [|1;2;3|])
 
-    testCase "Return FileDescription POST Test" <| fun _ ->
-        let file = api.PostApiReturnFileDescription()
-        file.Name.Value |> shouldEqual "1.txt"
-        file.Bytes.Value |> shouldEqual [|1uy;2uy;3uy|]
+    testCaseAsync "Return Seq Int POST Test" <| 
+        (api.PostApiReturnSeqInt()
+         |> asyncEqual [|1;2;3|])
+
+
+    testCaseAsync "Return Object Point GET Test" <| async {
+        let! point = api.GetApiReturnObjectPointClass()
+        point.X  |> shouldEqual (Some(0))
+        point.Y  |> shouldEqual (Some(0))
+    }
+
+    testCaseAsync "Return Object Point POST Test" <| async {
+        let! point = api.PostApiReturnObjectPointClass()
+        point.X  |> shouldEqual (Some(0))
+        point.Y  |> shouldEqual (Some(0))
+    }
+
+
+    testCaseAsync "Return FileDescription GET Test" <| async {
+        let! file = api.GetApiReturnFileDescription()
+        file.Name  |> shouldEqual (Some "1.txt")
+        file.Bytes  |> shouldEqual (Some [|1uy;2uy;3uy|])
+    }
+
+    testCaseAsync "Return FileDescription POST Test" <| async {
+        let! file = api.PostApiReturnFileDescription()
+        file.Name  |> shouldEqual (Some "1.txt")
+        file.Bytes  |> shouldEqual (Some [|1uy;2uy;3uy|])
+    }
   ]
