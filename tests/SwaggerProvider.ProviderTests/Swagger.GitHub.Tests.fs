5609657897113e962fecb549ed6a2f594004e568
Chet Husk <chusk3@gmail.com>
2018-01-30 13:22:14 -0600

    ignore github tests due to rate limits and default to async methods

--- tests/SwaggerProvider.ProviderTests/Swagger.GitHub.Tests.fs
+++ tests/SwaggerProvider.ProviderTests/Swagger.GitHub.Tests.fs
@@ -5,7 +5,7 @@ open Expecto
 
 let [<Literal>] Schema = __SOURCE_DIRECTORY__ + "/Schemas/GitHub.json"
 let [<Literal>] Host = "https://api.github.com"
-type GitHub = SwaggerProvider<Schema, OperationTypes = OperationTypes.Async>
+type GitHub = SwaggerProvider<Schema>
 let github = GitHub(Host, Headers = [|"User-Agent","Mozilla/5.0 (iPad; U; CPU OS 3_2_1 like Mac OS X; en-us) AppleWebKit/531.21.10 (KHTML, like Gecko) Mobile/7B405"|])
 
 type SyncGitHub = SwaggerProvider<Schema, OperationTypes = OperationTypes.Sync>
@@ -13,13 +13,13 @@ let syncGitHub = SyncGitHub(Host, Headers = [|"User-Agent","Mozilla/5.0 (iPad; U
 
 [<Tests>] // Explicit
 let githubTest =
-    testCaseAsync "All/Get fsprojects from GitHub" <| async {
+    ptestCaseAsync "All/Get fsprojects from GitHub" <| async {
         let! repos = github.OrgRepos("fsprojects")
         Expect.isGreaterThan repos.Length 0 "F# community is strong"
     }        
 
 [<Tests>]       
 let syncGitHubTest =
-    testCase "All/Get fsproject from GitHub sync" <| fun _ -> 
+    ptestCase "All/Get fsproject from GitHub sync" <| fun _ -> 
         let length = syncGitHub.OrgRepos("fsprojects").Length
         Expect.isGreaterThan length 0 "F# community is strong"
