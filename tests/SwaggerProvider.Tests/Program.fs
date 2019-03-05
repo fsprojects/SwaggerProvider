87bfb980ce0ce9a8303dd46024d8ceae83769a26
Sergey Tihon <sergey.tihon@gmail.com>
2019-03-06 00:19:36 +0300

    publish test results in NUnit format

--- tests/SwaggerProvider.Tests/Program.fs
+++ tests/SwaggerProvider.Tests/Program.fs
@@ -7,4 +7,7 @@ let main args =
     let config =
         { defaultConfig with
             verbosity = Logging.LogLevel.Verbose }
+    // TODO: Multiple results?
+    let writeResults = TestResults.writeNUnitSummary ("bin/TestResults-1.xml", "Expecto.Tests")
+    let config = config.appendSummaryHandler writeResults
     runTestsInAssembly config args
