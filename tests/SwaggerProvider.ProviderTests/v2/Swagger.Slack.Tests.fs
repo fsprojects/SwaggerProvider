68f67b1c43305fcd8a69f87b796f0462539a48e0
Sergey Tihon <sergey.tihon@gmail.com>
2019-10-29 21:05:57 +0300

    Repro test  for #126

---
+++ tests/SwaggerProvider.ProviderTests/v2/Swagger.Slack.Tests.fs
@@ -0,0 +1,32 @@
+module Swagger.Slack.Tests
+
+open SwaggerProvider
+open Expecto
+open System
+
+let [<Literal>] Schema = __SOURCE_DIRECTORY__ + "/../Schemas/v2/Slack.json"
+let [<Literal>] Host = "https://slack.com"
+type Slack = SwaggerClientProvider<Schema, PreferAsync = true>
+
+[<Tests>]
+let slackTests =
+  let slack = Slack.Client()
+  slack.HttpClient.BaseAddress <- Uri Host
+  
+  testList "All/TP Slack Tests" [
+    testCaseAsync "call provided methods" <| async {
+        let! result =
+          slack.ChatPostEphemeral(
+            threadTs = None,
+            blocks = "",
+            attachments = "",
+            asUser = None,
+            parse = "",
+            token = "",
+            text = "Hello",
+            user = "UNL9PF6P6",
+            linkNames = None,
+            channel = "CPDMKJR34") |> Async.Ignore
+        Expect.equal true true "wahoo"
+    }
+  ]
