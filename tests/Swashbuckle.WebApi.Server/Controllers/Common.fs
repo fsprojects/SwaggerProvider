8718e6f808746846f613599c5e0518e125987e84
Sergey Tihon <sergey.tihon@gmail.com>
2019-11-08 11:29:18 +0300

    MIME literals

---
+++ tests/Swashbuckle.WebApi.Server/Controllers/Common.fs
@@ -0,0 +1,27 @@
+﻿namespace Swashbuckle.WebApi.Server.Controllers
+
+module Application =
+    [<Literal>]
+    let OctetStream = "application/octet-stream"
+    [<Literal>]
+    let Json = "application/json"
+
+
+module Types =
+    open System.Runtime.Serialization
+
+    [<DataContract>]
+    type PointClass(x:int, y:int) =
+        new () = PointClass(0,0)
+        [<DataMember>]
+        member val X = x with get, set
+        [<DataMember>]
+        member val Y = y with get, set
+
+    [<DataContract>]
+    type FileDescription(name:string, bytes:byte[]) =
+        new () = FileDescription("",[||])
+        [<DataMember>]
+        member val Name = name with get, set
+        [<DataMember>]
+        member val Bytes = bytes with get, set
