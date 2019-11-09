b10a42b7363914ed43c213c9c313d550f25166e1
Sergey Tihon <sergey.tihon@gmail.com>
2019-11-10 00:31:26 +0300

    tests cleanup

--- tests/SwaggerProvider.ProviderTests/v3/Swashbuckle.FileController.Tests.fs
+++ tests/SwaggerProvider.ProviderTests/v3/Swashbuckle.FileController.Tests.fs
@@ -7,37 +7,36 @@ open Swashbuckle.v3.ReturnControllersTests
 
 [<Tests>]
 let resourceControllersTests =
+  let text1 = "This is test file content"
+  let text2 = "Another test file content"
+  let toStream (text:string) = 
+      let bytes = System.Text.Encoding.UTF8.GetBytes(text)
+      new MemoryStream(bytes)
+  let fromStream (stream:IO.Stream) = async {
+      use reader = new StreamReader(stream)
+      return! reader.ReadToEndAsync() |> Async.AwaitTask
+  }
+
   testList "All/v3/Swashbuckle.FileControllersTests.Tests" [
 
     testCaseAsync "Download file as IO.Stream" <| async {
         let! stream = api.GetApiReturnFile()
-        use reader = new StreamReader(stream)
-        let! text = reader.ReadToEndAsync() |> Async.AwaitTask
-        Expect.stringContains text "I am totally a file" "incorrect server response"
+        let! actual = fromStream stream
+        Expect.stringContains actual "I am totally a file" "incorrect server response"
     }
 
     testCaseAsync "Send file and get it back" <| async {
-        let text = "This is test file"
-        let bytes = System.Text.Encoding.UTF8.GetBytes(text)
-        let stream = new MemoryStream(bytes)
-
-        let data = WebAPI.OperationTypes.PostApiReturnFileSingle_formData(stream)
+        let data = WebAPI.OperationTypes.PostApiReturnFileSingle_formData(toStream text1)
         let! stream = api.PostApiReturnFileSingle(data)
-        use reader = new StreamReader(stream)
-        let! outText = reader.ReadToEndAsync() |> Async.AwaitTask
-        Expect.equal outText text "incorrect server response"
+        let! actual = fromStream stream
+        Expect.equal actual text1 "incorrect server response"
     }
 
     testCaseAsync "Send form-with-file and get it back as IO.Stream" <| async {
-        let text = "This is test file"
-        let bytes = System.Text.Encoding.UTF8.GetBytes(text)
-        let stream = new MemoryStream(bytes)
-
-        let data = WebAPI.OperationTypes.PostApiReturnFileFormWithFile_formData("newName.txt", stream)
+        let data = WebAPI.OperationTypes.PostApiReturnFileFormWithFile_formData("newName.txt", toStream text1)
         let! stream = api.PostApiReturnFileFormWithFile(data)
-        use reader = new StreamReader(stream)
-        let! outText = reader.ReadToEndAsync() |> Async.AwaitTask
-        Expect.equal outText text "incorrect server response"
+        let! actual = fromStream stream
+        Expect.equal actual text1 "incorrect server response"
     }
 
   ]
