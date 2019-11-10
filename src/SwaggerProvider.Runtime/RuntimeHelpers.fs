d0584ce2e9b0e1eb226ee2aaaf95d4f9703e1a4a
Sergey Tihon <sergey.tihon@gmail.com>
2019-11-10 13:00:42 +0300

    support for multiple files

--- src/SwaggerProvider.Runtime/RuntimeHelpers.fs
+++ src/SwaggerProvider.Runtime/RuntimeHelpers.fs
@@ -89,6 +89,9 @@ module RuntimeHelpers =
 
     let multipartFormDataContentFromObject (object:obj) =
         let cnt = new MultipartFormDataContent()
+        let addFileStream name (stream:IO.Stream) =
+            let filename = Guid.NewGuid().ToString() // asp.net core cannot deserialize IFormFile otherwise
+            cnt.Add(new StreamContent(stream), name, filename)
         if not <| isNull object then
             object.GetType().GetProperties(System.Reflection.BindingFlags.Public ||| System.Reflection.BindingFlags.Instance)
             |> Seq.iter (fun prop ->
@@ -98,11 +101,10 @@ module RuntimeHelpers =
                     | _ -> prop.Name
                 match prop.GetValue(object) with
                 | null -> ()
-                | :? IO.Stream as stream ->
-                    let filename = Guid.NewGuid().ToString() // asp.net core cannot deserialize IFormFile otherwise
-                    cnt.Add(new StreamContent(stream), name, filename)
+                | :? IO.Stream as stream -> addFileStream name stream
+                | :? (IO.Stream[]) as streams -> streams |> Seq.iter (addFileStream name)
                 | x ->
-                    let strValue = x.ToString() // TODO: serialize?
+                    let strValue = x.ToString() // TODO: serialize? does not work with arrays probably
                     cnt.Add(toStringContent strValue, name)
             )
         cnt
--- tests/Swashbuckle.WebApi.Server/Controllers/FileController.fs
+++ tests/Swashbuckle.WebApi.Server/Controllers/FileController.fs
@@ -22,13 +22,11 @@ type ReturnFileController () =
 
     [<HttpPost("single"); Produces(MediaTypes.ApplicationOctetStream, Type = typeof<FileResult>)>]
     member this.PostFile (file:IFormFile) :FileResult =
-        if isNull file then raise <| NullReferenceException("file is null")
         this.File(file.OpenReadStream(), MediaTypes.ApplicationOctetStream, file.FileName) :> FileResult
 
     [<HttpPost("multiple"); Produces(MediaTypes.ApplicationJson, Type=typeof<int>)>]
     member this.PostFiles (files:IFormFileCollection) =
-        if isNull files then raise <| NullReferenceException("files is null")
-        files.Count // ??? 0
+        files.Count // return 0 when you call from Swagger UI
 
     [<HttpPost("form-with-file"); Produces(MediaTypes.ApplicationOctetStream, Type = typeof<FileResult>)>]
     member this.PostFormWithFile ([<FromForm>] formWithFile:FormWithFile) :FileResult =
