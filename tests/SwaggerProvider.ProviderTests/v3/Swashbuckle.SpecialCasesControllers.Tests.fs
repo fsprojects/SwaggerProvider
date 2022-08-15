54966b94cf76c6dfabb02b370566c91af735df48
Sergey Tihon <sergey.tihon@gmail.com>
2022-08-15 20:36:46 +0200

    feat: add tests that repro #196

---
+++ tests/SwaggerProvider.ProviderTests/v3/Swashbuckle.SpecialCasesControllers.Tests.fs
@@ -0,0 +1,13 @@
+﻿module Swashbuckle.v3.SpecialCasesControllersTests
+
+open Expecto
+open System
+open Swashbuckle.v3.ReturnControllersTests
+
+[<Tests>]
+let resourceControllersTests =
+    testList "All/v3/Swashbuckle.SpecialCasesControllers.Tests" [
+
+        testCaseAsync "Requst response in JSON format from MultiFormatController"
+        <| async { do! api.GetApiMultiFormat() |> asyncEqual "0.0" }
+    ]
