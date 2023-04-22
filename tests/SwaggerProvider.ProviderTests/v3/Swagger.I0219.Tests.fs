f1c7ef2fdb4df64268a2db9543dd1ca7c85b745d
jkone27 <jkone27@users.noreply.github.com>
2023-04-22 19:12:36 +0200

    accept "default" as "ok response" fallback (#223)
    * accept "default" as "ok response" fallback
    
    * tried run fantomas but fails format on file
    
    * added testst
    
    * fix: format
    
    * fix: add new test to fsproj
    
    ---------
    
    Co-authored-by: Sergey Tihon <sergey.tihon@gmail.com>

---
+++ tests/SwaggerProvider.ProviderTests/v3/Swagger.I0219.Tests.fs
@@ -0,0 +1,10 @@
+module Swagger.I0219.Tests
+
+open SwaggerProvider
+
+[<Literal>]
+let Schema = __SOURCE_DIRECTORY__ + "/../Schemas/v3/issue219.yaml"
+
+type AcmeApi = OpenApiClientProvider<Schema>
+
+let inst = AcmeApi.Client()
