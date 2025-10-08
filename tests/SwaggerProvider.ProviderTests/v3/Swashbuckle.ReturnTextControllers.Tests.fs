ceee0a94ba48f9c80592d8d81af3931afbef6cc6
Sergey Tihon <sergey.tihon@gmail.com>
2025-10-08 22:25:35 +0200

    fix: do not compile client twice

--- tests/SwaggerProvider.ProviderTests/v3/Swashbuckle.ReturnTextControllers.Tests.fs
+++ tests/SwaggerProvider.ProviderTests/v3/Swashbuckle.ReturnTextControllers.Tests.fs
@@ -6,22 +6,7 @@ open SwaggerProvider
 open System
 open System.Net.Http
 
-type CallLoggingHandler(messageHandler) =
-    inherit DelegatingHandler(messageHandler)
-
-    override _.SendAsync(request, cancellationToken) =
-        printfn $"[SendAsync]: %A{request.RequestUri}"
-        base.SendAsync(request, cancellationToken)
-
-type WebAPI = OpenApiClientProvider<"http://localhost:5000/swagger/v1/openapi.json", IgnoreOperationId=true>
-
-let api =
-    let handler = new HttpClientHandler(UseCookies = false)
-
-    let client =
-        new HttpClient(handler, true, BaseAddress = Uri("http://localhost:5000"))
-
-    WebAPI.Client(client)
+open Swashbuckle.v3.ReturnControllersTests
 
 let asyncEqual expected actualTask =
     task {
