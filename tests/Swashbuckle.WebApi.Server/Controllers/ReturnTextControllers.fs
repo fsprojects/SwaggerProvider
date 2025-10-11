ad45d413bf58a1d558857c4e11e43fb76204b92a
Sergey Tihon <sergey.tihon@gmail.com>
2025-10-11 23:25:47 +0200

    fix: add csv output formatter

--- tests/Swashbuckle.WebApi.Server/Controllers/ReturnTextControllers.fs
+++ tests/Swashbuckle.WebApi.Server/Controllers/ReturnTextControllers.fs
@@ -1,6 +1,8 @@
 namespace Swashbuckle.WebApi.Server.Controllers
 
+open System.Text
 open Microsoft.AspNetCore.Mvc
+open Microsoft.AspNetCore.Mvc.Formatters
 open Swagger.Internal
 
 [<Route("api/[controller]")>]
@@ -16,3 +18,23 @@ type ReturnCsvController() =
     [<HttpGet; Consumes(MediaTypes.ApplicationJson); Produces("text/csv")>]
     member this.Get() =
         "Hello,world" |> ActionResult<string>
+
+// Simple CSV output formatter
+// This formatter assumes the controller returns a string (already CSV-formatted)
+type CsvOutputFormatter() as this =
+    inherit TextOutputFormatter()
+
+    do
+        this.SupportedMediaTypes.Add("text/csv")
+        this.SupportedEncodings.Add(Encoding.UTF8)
+        this.SupportedEncodings.Add(Encoding.Unicode)
+
+    override _.CanWriteType(t) =
+        // Accept string type only (for simplicity)
+        t = typeof<string>
+
+    override _.WriteResponseBodyAsync(context, encoding) =
+        let response = context.HttpContext.Response
+        let value = context.Object :?> string
+        let bytes = encoding.GetBytes(value)
+        response.Body.WriteAsync(bytes, 0, bytes.Length)
